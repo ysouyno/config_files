b5cf45e84bd2056ff70a1c473f835b86570c0297
ysouyno <ysouyno@163.com>
2020-01-17 22:24:56 +0800

    company-yasnippet can work with company-clang and company-c-headers

--- emacs/prelude/personal/custom.el
+++ emacs/prelude/personal/custom.el
@@ -1,3 +1,12 @@
+;;; company
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "\C-n") #'company-select-next)
+  (define-key company-active-map (kbd "\C-p") #'company-select-previous)
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  )
+(setq company-idle-delay 0.1)
+
 ;;; company-c-headers
 (prelude-require-package 'company-c-headers)
 
@@ -10,19 +19,29 @@
 (add-to-list 'company-c-headers-path-system "/usr/include/c++/9.2.0")
 (add-to-list 'company-c-headers-path-system ".")
 
-;;; company
-(with-eval-after-load 'company
-  (define-key company-active-map (kbd "\C-n") #'company-select-next)
-  (define-key company-active-map (kbd "\C-p") #'company-select-previous)
-  (define-key company-active-map (kbd "M-n") nil)
-  (define-key company-active-map (kbd "M-p") nil))
-
-(setq company-idle-delay 0.1)
+;;; yasnippet
+(prelude-require-packages '(yasnippet yasnippet-snippets))
 
 ;;; prelude-c
+(defun ysouyno-c-mode-config ()
+  (set (make-local-variable 'company-backends)
+       '((company-c-headers company-clang
+                            company-dabbrev-code company-yasnippet)))
+  )
+
 (defun ysouyno-c-mode-common-defaults ()
   (setq c-default-style "k&r"
         c-basic-offset 2)
+  (add-hook 'c++-mode-hook 'ysouyno-c-mode-config)
+  (add-hook 'c-mode-hook 'ysouyno-c-mode-config)
+  (yas-minor-mode 1)
   )
 
 (add-hook 'prelude-c-mode-common-hook 'ysouyno-c-mode-common-defaults t)
+
+;;; company-quickhelp
+(prelude-require-package 'company-quickhelp)
+(company-quickhelp-mode)
+
+;;; common
+(global-linum-mode t)
