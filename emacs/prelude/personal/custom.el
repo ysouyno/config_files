8d7746624ff4857d8bd829bd04531053a44df68e
ysouyno <ysouyno@163.com>
2020-03-07 09:31:44 +0800

    disable whitespace-line

--- emacs/prelude/personal/custom.el
+++ emacs/prelude/personal/custom.el
@@ -83,3 +83,10 @@
     (setenv "PATH" (mapconcat 'identity
                               (delete-dups path) path-separator)))
   )
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(whitespace-line ((t nil))))
