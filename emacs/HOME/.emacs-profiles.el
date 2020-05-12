ecbd5d09b988679f17cf3093a8b52248a84408e8
ysouyno <ysouyno@163.com>
2020-05-12 22:53:37 +0800

    doom-emacs fresh installed and chemacs config ok

---
+++ emacs/HOME/.emacs-profiles.el
@@ -0,0 +1,4 @@
+(("default" . ((user-emacs-directory . "~/prelude")))
+ ("doom" . ((user-emacs-directory . "~/doom-emacs")
+            (env . (("DOOMDIR" . "~/.doom.d")))))
+ ("spacemacs" . ((user-emacs-directory . "~/spacemacs"))))
